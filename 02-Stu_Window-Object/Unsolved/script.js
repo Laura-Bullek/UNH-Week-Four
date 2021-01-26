362a0c5391da3bb89aaef02d14827162180d58fb
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-01-26 12:43:39 -0500

    Completed 02-Stu_Window-Object project

--- 02-Stu_Window-Object/Unsolved/script.js
+++ 02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,19 @@
 // TODO: What will the following line of code log?
 console.log(window);
+// It traverses the DOM to the window
 
 // TODO: What will the following line of code log?
 console.log(window.document);
+// It traverses the DOM to the HTML document of the page
 
 // TODO: What will the following line of code log?
 console.log(document.documentElement);
+// It traverses the DOM to the elements of the document
 
 // TODO: What will the following line of code log?
 console.log(document.head);
+// Returns the head element of the current document.
 
+// BONUS:
+// How would you log the length property of the window object?
+console.log(window.length);
