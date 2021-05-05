a0a1c8f3b22ca7ddb63542278676148df3104a9b
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-05-04 21:58:25 -0400

    Added all solved project folders

--- 22-Stu_Local-Storage/Solved/assets/js/script.js
+++ 22-Stu_Local-Storage/Solved/assets/js/script.js
@@ -5,6 +5,7 @@ var msgDiv = document.querySelector("#msg");
 var userEmailSpan = document.querySelector("#user-email");
 var userPasswordSpan = document.querySelector("#user-password");
 
+
 renderLastRegistered();
 
 function displayMessage(type, message) {
@@ -13,9 +14,15 @@ function displayMessage(type, message) {
 }
 
 function renderLastRegistered() {
-  // TODO: Retrieve the last email and password and render it to the page
-  localStorage.getItem ("last-email", emailInput);
-  localStorage.getItem ("last-password", passwordInput);
+  var email = localStorage.getItem("email");
+  var password = localStorage.getItem("password");
+
+  if (!email || !password) {
+    return;
+  }
+
+  userEmailSpan.textContent = email;
+  userPasswordSpan.textContent = password;
 }
 
 signUpButton.addEventListener("click", function(event) {
@@ -31,12 +38,8 @@ signUpButton.addEventListener("click", function(event) {
   } else {
     displayMessage("success", "Registered successfully");
 
-  // TODO: Save email and password to localStorage and render the last registered user
-localStorage.setItem("email", email);
-userEmailSpan.textContent = email;
-localStorage.setItem("password", password);
-userPasswordSpan.textContent = password;
-renderLastRegistered();
-}
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    renderLastRegistered();
+  }
 });
-
